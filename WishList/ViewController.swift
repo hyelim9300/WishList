b77665c11299eb2592264c0d8e39fbc21df82993
hyelim9300 <hye930011@naver.com>
2024-04-12 19:56:19 +0900

    Initial Commit

---
+++ WishList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WishList
+//
+//  Created by 서혜림 on 4/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
